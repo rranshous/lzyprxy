8d16a2e724a804180779e0734f73af0ca6424edc
Robby Ranshous <rranshous@gmail.com>
2015-08-09 13:44:54 -0400

    simple func

--- atc.go
+++ atc.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"github.com/rubyist/circuitbreaker"
+	"time"
 )
 
 type Message struct {
@@ -11,9 +12,9 @@ type Message struct {
 	msg    string
 }
 
-func (m *Message) Reply(r string) bool {
+func (m *Message) Reply(r string, rc chan Message) bool {
 	m.rc <- Message{
-		rc:     m.rc,
+		rc:     rc,
 		target: m.target,
 		msg:    r}
 	return true
@@ -24,18 +25,39 @@ type AirTrafficControl struct {
 	rc    chan Message
 }
 
+func runAirTrafficControl(rc chan Message) bool {
+	defer close(rc) // will prob trip of sender, add flag ?
+	panel := circuit.NewPanel()
+	for {
+		msg := <-rc
+		cb, ok := panel.Get(msg.target)
+		switch msg.msg {
+		case "JOIN":
+			// add new breaker for this target if first time seen
+			if !ok {
+				// panel.Add(msg.target, circuit.NewThresholdBreaker(1))
+				panel.Add(msg.target, circuit.NewRateBreaker(0.10, 10))
+			}
+			if cb.Ready() {
+				msg.Reply("CLEARED", rc)
+			} else {
+				msg.Reply("ABORT", rc)
+			}
+		case "SUCCESS":
+			cb.Success()
+		case "FAILURE":
+			cb.Fail()
+		}
+
+	}
+	return true
+}
+
 func NewAirTrafficControl() *AirTrafficControl {
 	atc := &AirTrafficControl{
 		panel: circuit.NewPanel(),
 		rc:    make(chan Message)}
-	go func() {
-		defer close(atc.rc) // will prob trip of sender, add flag ?
-		for {
-			msg := <-atc.rc
-			msg.Reply("WAIT")
-			msg.Reply("CLEARED")
-		}
-	}()
+	go runAirTrafficControl(atc.rc)
 	return atc
 }
 
@@ -49,28 +71,36 @@ func (atc *AirTrafficControl) JoinQueue(h string) chan Message {
 }
 
 func main() {
+	fmt.Println("--TESTING")
+
 	atc := NewAirTrafficControl()
-	rc := atc.JoinQueue("testhost")
-	defer close(rc)
-	abort := false
-	waiting := true
-	for abort == false && waiting == true {
-		msg := <-rc
-		switch msg.msg {
-		case "WAIT":
-			fmt.Println("waiting")
-			waiting = true
-		case "ABORT":
-			fmt.Println("aborting")
-			abort = true
-		case "CLEARED":
-			fmt.Println("cleared")
-			waiting = false
+	for i := 0; i < 11113; i++ {
+		time.Sleep(1000000)
+
+		rc := atc.JoinQueue("testtarget")
+		defer close(rc)
+
+		abort := false
+		waiting := true
+		var msg Message
+		for abort == false && waiting == true {
+			msg = <-rc
+			switch msg.msg {
+			case "WAIT":
+				waiting = true
+			case "ABORT":
+				abort = true
+			case "CLEARED":
+				waiting = false
+			}
 		}
+		if !abort {
+			fmt.Println("we're a go!", i)
+			msg.Reply("FAILURE", rc)
+		} else {
+			fmt.Println("we've aborted!", i)
+		}
+		time.Sleep(5)
 	}
-	if !abort {
-		fmt.Println("we're a go!")
-	} else {
-		fmt.Println("we've aborted!")
-	}
+	fmt.Println("--DONE TESTING")
 }
